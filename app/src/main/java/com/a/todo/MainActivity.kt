544da9da8b0bc231b078f9dc414646641edcaf5f
Bashorun Olajide <bashorun.olajide@gmail.com>
2021-01-09 06:26:27 +0100

    Initial commit

---
+++ app/src/main/java/com/a/todo/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.a.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment) )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment);
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
