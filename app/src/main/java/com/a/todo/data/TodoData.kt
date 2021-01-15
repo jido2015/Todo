9730afb3dbe05b6b4a12968209deca5fdf58d996
Bashorun Olajide <bashorun.olajide@gmail.com>
2021-01-15 21:16:20 +0100

    Create Entity class (TodoData)

---
+++ app/src/main/java/com/a/todo/data/TodoData.kt
@@ -0,0 +1,14 @@
+package com.a.todo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName= "todo_table")
+data class TodoData (
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+        )
