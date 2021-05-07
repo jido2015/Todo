071e0583faa0bdd623679666ce3320fb765a6587
Bahorun Olajide <bashorunolajide@gmail.com>
2021-05-07 10:50:10 +0100

    Add phone number formatter test

--- app/src/test/java/com/a/todo/UnitTest.kt
+++ app/src/test/java/com/a/todo/UnitTest.kt
@@ -5,4 +5,9 @@ import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class UnitTest {
+    @Test
+    fun formatPhoneNumberTest() {
+        assertEquals("23408090712213", HelperClass.phoneNumberFormat("08090712213"))
+    }
+
 }
